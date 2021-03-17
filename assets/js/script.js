af1f0c699a9c918113a16949f2189732ae79cc6f
ankitmakavanawebiots <ankit.makvana@webiots.in>
2021-03-17 16:58:21 +0530

    initial changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,3 +63,57 @@ $('.advisory-box').on('click', function(e) {
 
 });
 
+
+
+/*=====================
+    03. advisory-box
+==========================*/
+
+$('.owl-carousel').owlCarousel({
+    // margin:40,
+    loop:true,
+    // autoWidth:true,
+    items:3.3,
+    dots: false,
+    nav : true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        320:{
+            items:1
+        },
+        768:{
+            items:1.3
+        },
+        805:{
+            items:1.5
+        },
+        900:{
+            items:1.7
+        },
+        970:{
+            items:2
+        },
+        992:{
+            items:1.8
+        },
+        1150:{
+            items:2.3
+        },
+        1300:{
+            items:2.5
+        },
+        1470:{
+            items:2.8
+        },
+        1530:{
+            items:3
+        },
+        1650:{
+            items:3.3
+        }
+    }
+})
+
